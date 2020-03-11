0624737e643f8c06b40a8dde62c0f2fdf2e9318b
Mathias <mathias@angleborn.net>
2020-03-11 21:15:11 +0100

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -15,31 +15,13 @@ def open_connection():
     return connection
 
 def execute_sql():
-    connection = open_connection()
-    sql
-    values()
-    commit = False
-    single = False
-    cursor = connection.execute(sql, values)
-    if cursor.commit:
-        results = connction.commit()
-    elif cursor.fetchone() = single:
-        results = ternary
-    else:
-        results = cursor.fetchall()
-    cursor.close()
-    return results
+
 
 @app.teardown_appcontext
 def close_connetion(exeption):
     connection = getattr(g, '_connection', None)
     if connection != None:
         connection.close()
-
-
-
-
-
 
 
 @app.route('/')
